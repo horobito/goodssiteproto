6609c723961ce7d1c4f19a1a7780f989732e19a4
horobito <odkfu1@gmail.com>
2022-03-10 17:26:01 +0900

    feat : set init

---
+++ src/main/java/com/prototype/GoodssiteprotoApplication.java
@@ -0,0 +1,13 @@
+package com.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodssiteprotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodssiteprotoApplication.class, args);
+    }
+
+}
