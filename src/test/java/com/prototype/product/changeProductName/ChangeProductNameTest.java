010baf12b9cfa7f32de42ee3f2eeff7c494c5772
horobito <odkfu1@gmail.com>
2022-03-22 20:02:59 +0900

    test : create [ChangeProductName Test 3. Abnormal Condition - Long Name ]

--- src/test/java/com/prototype/product/changeProductName/ChangeProductNameTest.java
+++ src/test/java/com/prototype/product/changeProductName/ChangeProductNameTest.java
@@ -40,11 +40,14 @@ public class ChangeProductNameTest {
         when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
         when(productRepository.save(any())).thenReturn(productHelper);
 
-        sut.changeProductName(1L, "newName");
+        String newName = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+
+
+        sut.changeProductName(1L, newName);
         verify(productRepository, times(1)).save(any());
     }
 
-    @DisplayName("ChangeProductName Test 1. Abnormal Condition - empty Name ")
+    @DisplayName("ChangeProductName Test 2. Abnormal Condition - empty Name ")
     @Test
     public void test2() {
         ProductService sut = new ProductService(productRepository);
@@ -62,5 +65,28 @@ public class ChangeProductNameTest {
         when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
         assertThrows(IllegalArgumentException.class,
                 ()->sut.changeProductName(1L, ""));
+    }
+
+    @DisplayName("ChangeProductName Test 3. Abnormal Condition - Long Name ")
+    @Test
+    public void test3() {
+        ProductService sut = new ProductService(productRepository);
+
+        String productName = "testName 1";
+        int productPrice = 1;
+        int stock = 1;
+        boolean isStockInfinite = false;
+        Long userId = 1L;
+
+        ProductHelper productHelper = ProductHelper.create(
+                1L, ProductName.create(productName), ProductPrice.create(productPrice),
+                SellerId.create(userId), Stock.create(stock, isStockInfinite)
+        );
+
+        String newName = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890*";
+
+        when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
+        assertThrows(IllegalArgumentException.class,
+                ()->sut.changeProductName(1L, newName));
     }
 }
