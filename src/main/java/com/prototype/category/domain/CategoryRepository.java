d92a9592cb222c017ec619ad2647c20df4da9902
horobito <odkfu1@gmail.com>
2022-03-22 20:48:57 +0900

    refact : change method name

--- src/main/java/com/prototype/category/domain/CategoryRepository.java
+++ src/main/java/com/prototype/category/domain/CategoryRepository.java
@@ -4,13 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByCategoryNameIn(List<CategoryName> categoryNameList);
 
-    Optional<Category> findByCategoryIdAndDeleted(Long categoryId, boolean b);
+    Optional<Category> findByCategoryIdAndIsDeleted(Long categoryId, boolean b);
 }
 
 
