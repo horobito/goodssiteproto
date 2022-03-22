cd0e533f3fdb5feb18ff0b6de76bd014c31bb3ec
horobito <odkfu1@gmail.com>
2022-03-22 22:54:10 +0900

    test: create [Revive Test1 : Normal Condition]

---
+++ src/test/java/com/prototype/category/revive/ReviveTest.java
@@ -0,0 +1,46 @@
+package com.prototype.category.revive;
+
+import com.prototype.category.CategoryHelper;
+import com.prototype.category.domain.Category;
+import com.prototype.category.domain.CategoryName;
+import com.prototype.category.domain.CategoryRepository;
+import com.prototype.category.service.CategoryService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ReviveTest {
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+
+    @DisplayName("Revive Test1 : Normal Condition")
+    @Test
+    public void test1(){
+        CategoryService sut = new CategoryService(categoryRepository);
+
+
+        CategoryName categoryName = CategoryName.create("category1");
+
+        Long categoryId = 1L;
+
+        Category category = CategoryHelper.create(
+                categoryId, categoryName
+        );
+        category.delete();
+
+        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
+        sut.revive(categoryId);
+        verify(categoryRepository, times(1)).save(any());
+    }
+}
