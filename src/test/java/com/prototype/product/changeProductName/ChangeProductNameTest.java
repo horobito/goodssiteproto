14702f28507284f637d72135e2b631851f5ab2a7
horobito <odkfu1@gmail.com>
2022-03-22 19:59:33 +0900

    test : create [ChangeProductName Test 1. Normal Condition]

---
+++ src/test/java/com/prototype/product/changeProductName/ChangeProductNameTest.java
@@ -0,0 +1,45 @@
+package com.prototype.product.changeProductName;
+
+import com.prototype.product.ProductHelper;
+import com.prototype.product.domain.*;
+import com.prototype.product.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ChangeProductNameTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+
+    @DisplayName("ChangeProductName Test 1. Normal Condition ")
+    @Test
+    public void test1() {
+        ProductService sut = new ProductService(productRepository);
+
+        String productName = "testName 1";
+        int productPrice = 1;
+        int stock = 1;
+        boolean isStockInfinite = false;
+        Long userId = 1L;
+
+        ProductHelper productHelper = ProductHelper.create(
+                1L, ProductName.create(productName), ProductPrice.create(productPrice),
+                SellerId.create(userId), Stock.create(stock, isStockInfinite)
+        );
+        when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
+        when(productRepository.save(any())).thenReturn(productHelper);
+
+        sut.changeProductName(1L, "newName");
+        verify(productRepository, times(1)).save(any());
+    }
+}
