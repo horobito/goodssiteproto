77cbe0da7509c8717528b9bba262442b75593520
horobito <odkfu1@gmail.com>
2022-03-23 20:29:54 +0900

    test: create new method

--- src/main/java/com/prototype/category/domain/CategoryRepository.java
+++ src/main/java/com/prototype/category/domain/CategoryRepository.java
@@ -1,13 +1,19 @@
 package com.prototype.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByCategoryNameIn(List<CategoryName> categoryNameList);
+
+    @Query(value = "select c.*\n" +
+            "from categoryd c\n" +
+            "    left join category_product_relation cpr on cpr.product_id=:productId and cpr.is_deleted=false\n" +
+            "where c.category_id = cpr.category_id and c.is_deleted = false", nativeQuery = true)
+    List<Category> findByRelatedCategory(Long productId);
 
 }
 
