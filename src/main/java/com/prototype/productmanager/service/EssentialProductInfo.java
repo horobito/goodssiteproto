708a40969ee921e02d5ee4655e1d350a2208b508
horobito <odkfu1@gmail.com>
2022-03-23 20:27:52 +0900

    feat: create[EssentialProductInfo] class

---
+++ src/main/java/com/prototype/productmanager/service/EssentialProductInfo.java
@@ -0,0 +1,24 @@
+package com.prototype.productmanager.service;
+
+import com.prototype.category.service.CategoryDto;
+import lombok.Value;
+
+import java.util.List;
+
+@Value
+public class EssentialProductInfo {
+    Long productId;
+
+    String productName;
+
+    Long sellerId;
+
+    String sellerName;
+
+    int productPrice;
+
+    boolean isSoldOut;
+
+    List<CategoryDto> categoryDtos;
+
+}
