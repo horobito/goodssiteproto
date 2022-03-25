1aabe1045d6228098f206e3394ab5dfc2000f50c
horobito <odkfu1@gmail.com>
2022-03-25 20:11:27 +0900

    test : apply [ProductService] and [Product]'s change

---
+++ src/test/java/com/prototype/product/changeProductInfo/ChangeProductStockAmountTest.java
@@ -0,0 +1,149 @@
+package com.prototype.product.changeProductInfo;
+
+import com.prototype.product.ProductHelper;
+import com.prototype.product.domain.*;
+import com.prototype.product.service.ProductService;
+import com.prototype.user.service.UserDto;
+import com.prototype.user.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ChangeProductStockAmountTest {
+
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    UserService userService;
+
+    @DisplayName("Change Stock  Test 1. Normal Condition")
+    @Test
+    public void test1() {
+        ProductService sut = new ProductService(productRepository, userService);
+
+        String productName = "testName 1";
+        int productPrice = 1;
+        int stock = 1;
+        boolean isStockInfinite = false;
+        Long sellerId = 1L;
+
+        ProductHelper productHelper = ProductHelper.create(
+                1L, ProductName.create(productName), ProductPrice.create(productPrice),
+                SellerId.create(sellerId), Stock.create(stock, isStockInfinite)
+        );
+
+        UserDto userDto = new UserDto(
+                sellerId,
+                "user1",
+                "password1",
+                "MALE",
+                false,
+                LocalDate.now()
+        );
+
+        int changedStockAmount = 2;
+        String newName = "newName";
+        int newPrice = 1;
+        boolean newIsStockInfinite = false;
+
+        when(userService.getLoggedInUser()).thenReturn(userDto);
+        when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
+        when(productRepository.save(any())).thenReturn(productHelper);
+
+        sut.update(1L, newName, newPrice, changedStockAmount, newIsStockInfinite);
+        verify(productRepository, times(1)).save(any());
+
+
+    }
+
+    @DisplayName("Change Stock 2. Normal Condition - new stock amount is zero")
+    @Test
+    public void test2() {
+        ProductService sut = new ProductService(productRepository, userService);
+
+        String productName = "testName 1";
+        int productPrice = 1;
+        int stock = 1;
+        boolean isStockInfinite = false;
+        Long sellerId = 1L;
+
+        ProductHelper productHelper = ProductHelper.create(
+                1L, ProductName.create(productName), ProductPrice.create(productPrice),
+                SellerId.create(sellerId), Stock.create(stock, isStockInfinite)
+        );
+
+        int changedStockAmount = 0;
+        String newName = "newName";
+        int newPrice = 1;
+        boolean newIsStockInfinite = false;
+
+        UserDto userDto = new UserDto(
+                sellerId,
+                "user1",
+                "password1",
+                "MALE",
+                false,
+                LocalDate.now()
+        );
+
+        when(userService.getLoggedInUser()).thenReturn(userDto);
+        when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
+        when(productRepository.save(any())).thenReturn(productHelper);
+
+        sut.update(1L, newName, newPrice, changedStockAmount, newIsStockInfinite);
+        verify(productRepository, times(1)).save(any());
+
+
+    }
+
+    @DisplayName("Change Stock 3. Abnormal Condition - sum is minus")
+    @Test
+    public void test3() {
+        ProductService sut = new ProductService(productRepository, userService);
+
+        String productName = "testName 1";
+        int productPrice = 1;
+        int stock = 1;
+        boolean isStockInfinite = false;
+        Long sellerId = 1L;
+
+        ProductHelper productHelper = ProductHelper.create(
+                1L, ProductName.create(productName), ProductPrice.create(productPrice),
+                SellerId.create(sellerId), Stock.create(stock, isStockInfinite)
+        );
+
+        int changedStockAmount = -1;
+        String newName = "newName";
+        int newPrice = 1;
+        boolean newIsStockInfinite = false;
+
+
+        UserDto userDto = new UserDto(
+                sellerId,
+                "user1",
+                "password1",
+                "MALE",
+                false,
+                LocalDate.now()
+        );
+
+        when(userService.getLoggedInUser()).thenReturn(userDto);
+        when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
+
+        assertThrows(IllegalArgumentException.class,
+                ()->sut.update(1L, newName, newPrice, changedStockAmount, newIsStockInfinite));
+
+
+    }
+}
--- src/test/java/com/prototype/product/revive/ReviveTest.java
+++ src/test/java/com/prototype/product/revive/ReviveTest.java
@@ -3,12 +3,15 @@ package com.prototype.product.revive;
 import com.prototype.product.ProductHelper;
 import com.prototype.product.domain.*;
 import com.prototype.product.service.ProductService;
+import com.prototype.user.service.UserDto;
+import com.prototype.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -21,17 +24,29 @@ public class ReviveTest {
     @Mock
     ProductRepository productRepository;
 
+    @Mock
+    UserService userService;
+
 
     @DisplayName("Revive Test 1. Normal Condition")
     @Test
     public void test1() {
-        ProductService sut = new ProductService(productRepository);
+        ProductService sut = new ProductService(productRepository, userService);
 
         String productName = "testName 1";
         int productPrice = 1;
         int stock = 1;
         boolean isStockInfinite = false;
         Long userId = 1L;
+
+        UserDto userDto = new UserDto(
+                1L,
+                "user1",
+                "password1",
+                "MALE",
+                false,
+                LocalDate.now()
+        );
 
         ProductHelper productHelper = ProductHelper.create(
                 1L, ProductName.create(productName), ProductPrice.create(productPrice),
@@ -40,6 +55,7 @@ public class ReviveTest {
         productHelper.delete();
         when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
         when(productRepository.save(any())).thenReturn(productHelper);
+        when(userService.getLoggedInUser()).thenReturn(userDto);
 
         sut.revive(1L);
         verify(productRepository, times(1)).save(any());
@@ -48,7 +64,7 @@ public class ReviveTest {
     @DisplayName("Revive Test 2. Abnormal Condition - non deleted")
     @Test
     public void test2() {
-        ProductService sut = new ProductService(productRepository);
+        ProductService sut = new ProductService(productRepository, userService);
 
         String productName = "testName 1";
         int productPrice = 1;
@@ -56,11 +72,23 @@ public class ReviveTest {
         boolean isStockInfinite = false;
         Long userId = 1L;
 
+        UserDto userDto = new UserDto(
+                1L,
+                "user1",
+                "password1",
+                "MALE",
+                false,
+                LocalDate.now()
+        );
+
         ProductHelper productHelper = ProductHelper.create(
                 1L, ProductName.create(productName), ProductPrice.create(productPrice),
                 SellerId.create(userId), Stock.create(stock, isStockInfinite)
         );
         when(productRepository.findById(any())).thenReturn(Optional.of(productHelper));
+        when(userService.getLoggedInUser()).thenReturn(userDto);
+
+
         assertThrows(IllegalArgumentException.class, ()->sut.revive(1L));
 
     }
