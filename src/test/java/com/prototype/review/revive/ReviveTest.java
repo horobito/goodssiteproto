0cb742baaaf8df67b68b8a6fac15e4588843e668
horobito <odkfu1@gmail.com>
2022-03-28 15:18:45 +0900

    test : creat [Revive test 3. Abnormal Condition : non deleted ]

--- src/test/java/com/prototype/review/revive/ReviveTest.java
+++ src/test/java/com/prototype/review/revive/ReviveTest.java
@@ -111,6 +111,43 @@ public class ReviveTest {
 
     }
 
+    @DisplayName("Revive test 3. Abnormal Condition : non deleted ")
+    @Test
+    public void test3(){
+        ReviewService sut = new ReviewService(reviewRepository, userService);
 
+        Long loggedInUserId =1L;
+        String loggedInUserName = "user";
+        String loggedINUserPassword = "password";
+        String loggedInUserGender = "MALE";
+
+        Long productId = 1L;
+
+        UserDto loggedInUser = new UserDto(
+                loggedInUserId,
+                loggedInUserName,
+                loggedINUserPassword,
+                loggedInUserGender,
+                false,
+                LocalDate.now());
+
+        Long reviewerId = 2L;
+
+        String comment = "comment";
+        int score = 10;
+
+        Review review = Review.create(
+                productId, reviewerId, ReviewComment.create(comment), ProductScore.create(score)
+        );
+
+        assertFalse(review.isDeleted());
+
+        when(userService.getLoggedInUser()).thenReturn(loggedInUser);
+        when(reviewRepository.findById(any())).thenReturn(Optional.of(review));
+
+        assertThrows(IllegalArgumentException.class,
+                ()->sut.revive(productId));
+
+    }
 
 }
